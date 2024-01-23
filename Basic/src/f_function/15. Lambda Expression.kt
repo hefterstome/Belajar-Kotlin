6cbdbe3ef4e9e5d3e74971ff4780d3cc05a9a3a7
hefterstome <iqbra127@gmail.com>
2024-01-23 16:30:01 +0700

    Menyelesaikan Kotlin Dasar

--- Basic/src/f_function/15. Lambda Expression.kt
+++ Basic/src/f_function/15. Lambda Expression.kt
@@ -1,3 +1,54 @@
 package f_function
 
-/* LAMBDA EXPRESSION: function yang tidak memiliki nama */
+/* Function di Kotlin dianggap first-class citizens, artinya dianggap seperti tipe data lainnya.
+   > Dalam pembuatannya function bisa berdiri sendiri tanpa membutuhkan class (konsep oop Java)
+   > Bisa disimpan di variabel, array, bahkan dikirim ke parameter function itu sendiri (high-order)
+
+   LAMBDA EXPRESSION: function yang tidak memiliki nama.
+   -> Ketika menggunakan lambda, tidak harus mendeskripsikan nama functionnya
+   -> Lambda expression akan mengembalikan nilai kode baris terakhir
+    > Cocok digunakan untuk proses-proses sederhana/tidak rumit
+
+   ** Pembuatan Lambda
+                         ====== FUNCTION ======    ==== LAMBDA ====
+      val/var namaLambda: (tipe) -> tipe return = { parameter: tipe ->
+         kode program
+      }
+      val contohLambda: (String) -> String = {value: String ->
+         val result = "$value"
+         result
+      }
+
+*/
+fun main() {
+    /* CONTOH LAMBDA */
+    val namaLengkapLambda: (String, String) -> String = {namaAwal: String, namaAkhir: String ->
+        "$namaAwal $namaAkhir"
+//        val result = "$namaAwal $namaAkhir"
+//        result
+    }
+    val hasil = namaLengkapLambda("Iqbra", "Kurniawan")
+    println(hasil)
+    println(namaLengkapLambda("Iqbra", "Kurniawan"))
+
+    /* IT: kata kunci yang digunakan untuk merepresentasikan parameter lambda apabila hanya 1.
+       -> Hanya bisa digunakan ketika parameter 1
+       -> Jika parameter dinamai, tidak bisa digunakan
+     */
+    val itLambda: (String) -> String = {
+        "it = $it"
+    }
+    val upperItLambda: (String) -> String = {
+        it.uppercase()
+    }
+    println(itLambda("Kotlin"))
+    println(upperItLambda("Kotlin"))
+
+    /* METHOD REFERENCES: lambda yang mereferensi function yang sudah ada
+        > Ada function, kemudian membuat lambda untuk function tersebut
+       -> Pada body lambda cukup menggunakan ::namaFunction
+    */
+    fun toUpperFunc(value: String): String = value.uppercase()
+    val toUpperLambda: (String) -> String = ::toUpperFunc
+    println(toUpperLambda("Belajar Kotlin Masszeeeh"))
+}
--- Basic/src/f_function/17. Inline Function.kt
+++ Basic/src/f_function/17. Inline Function.kt
@@ -1,2 +1,42 @@
 package f_function
 
+/* High order funcion memiliki kelemahan jika dilakukan berulang-ulang dalam jumlah banyak akan menyebabkan performa turun.
+   > Hal ini dikarenakan dalam Java, saat compile ke binary akan membuat objek berulang-ulang.
+   > Hal ini dapat diatasi dengan inline function
+
+   INLINE FUNCTION: function yang memiliki kemampuan ketika compile, objek akan diduplikat.
+    > Ketika menggunakan inline func, objek saat compile akan diduplikat sehingga saat runtime tidak akan membuat objek lagi
+   -> Tidak ada syarat khusus untuk menjadikan inline
+   -> Cara membuatnya dengan menambahkan kata inline sebelum fun
+   -> Penggunaan inline perlu diperhatikan karena akan menambah kapasitas binary code meskipun performa akan semakin bagus
+    > Ketika proses yang berjalan sederhana, tidak akan memiliki efek besar
+    > Mirip konsep dengan recursive dan tail recursive
+
+   ** Cara mengecek
+      Tools > Kotlin > Show kotlin bytecode > Decompile
+*/
+inline fun helloInlineFunc(value: () -> String): String {
+//fun helloInlineFunc(value: () -> String): String {
+    return "Hello ${value()}"
+}
+
+/* NOINLINE: kunci yang digunakan agar parameter tidak menjadi inline
+    > Jika menggunakan inline, maka semua akan diduplikat
+    > Dengan menambahkan noinlin, maka objek tersebut akan tetap dibuat objek berulang-ulang dan tidak diduplikat
+*/
+inline fun helloNoinline(namaAwal: () -> String,
+                         noinline namaAkhir: () -> String): String {
+    return "Hello, ${namaAwal()} ${namaAkhir()}"
+}
+fun main() {
+    println(helloInlineFunc { "Iqbra" })
+    println(helloInlineFunc { "Kotlin" })
+
+    for (i in 0..100){
+        println(helloInlineFunc { "Inline" })
+    }
+
+    for (i in 0..100){
+        println(helloNoinline({"Iqbra"},{"Kurniawan"}))
+    }
+}
