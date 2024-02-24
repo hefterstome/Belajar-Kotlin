5d36c5d22504289747bd4391845ec18fab084482
hefterstome <iqbra127@gmail.com>
2024-02-24 21:04:10 +0700

    commit generic

---
+++ Generic/src/app/6. Covariant.kt
@@ -0,0 +1,7 @@
+package app
+
+/*
+ ** COVARIANT: artinya kita bisa mensubtitusi object parent dan class
+    > Menggunakan kata kunci out
+    > Tidak semua class generic bisa menerapkan covariant, hanya class generic yang memiliki function dengan return type function
+ */
