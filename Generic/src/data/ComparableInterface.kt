a619210cc30dd74d70592a89de08525e22bfd08e
hefterstome <iqbra127@gmail.com>
2024-02-26 19:49:36 +0700

    Kotlin Generic

--- Generic/src/data/ComparableInterface.kt
+++ Generic/src/data/ComparableInterface.kt
@@ -1,4 +1,7 @@
 package data
 
-class ComparableInterface {
+class ComparableInterface(val name: String, val quantity: Int): Comparable<ComparableInterface> {
+    override fun compareTo(other: ComparableInterface): Int { //Harus dioverride karena interface Comprable punya func compareTo
+        return quantity.compareTo(other.quantity)
+    }
 }
